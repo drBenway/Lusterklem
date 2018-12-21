d03fb3c3614ce4d90ea7487b4675228818e8c8ab
Yves Peeters2 <Yves.peeters2@ing.be>
2018-12-21 11:05:33 +0100

    improved code coverage, updated the app overview page

--- src/lusterklem-app.js
+++ src/lusterklem-app.js
@@ -10,9 +10,30 @@ class LusterklemApp extends LitElement {
   render() {
     return html`
       <style>
+       :host{
+        padding: 10px;
+       }
         ${styles}
       </style>
-      ${this.qrcode()}
+      <table class="overview_table">
+     
+      <tr>
+      <th>custom tag</th>
+      <th>info</th>
+      <th>example</th>
+    </tr>
+
+      <tr>
+      <td>&lt;lc-qr typenumber="0" qrdata="https://github.com/drBenway/Lusterklem" &gt; &lt/lc-qr&gt;*</td>
+      <td>Generates a qr code. takes a typenumber property (default 0), qrdata and errorcorrection. <span style="color: red"> Beware: the component is not responsive to width/ height as the qr code gets calculated based on the type and data</span></td>
+      <td>${this.qrcode()}</td>
+</tr>
+      
+</table>
+<p>
+* this component is a wrapper around the qr code @ https://kazuhikoarase.github.io/qrcode-generator/js/demo/
+</p>
+      
     `;
   }
 }
